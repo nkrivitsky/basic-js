32c246881b56eb987af357dd085f4940ce9bb56a
Nikita krivitsky <nikdemoth1@gmail.com>
2020-10-05 22:17:32 +0300

    feat: implement Count cats function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function countCats(array) {
+    if (!Array.isArray(array) || array.length == 0) return 0
+    let count = 0
+    array.forEach(element => {
+        if (Array.isArray(element)) {
+            count += countCats(element)
+        } else {
+            if (element == "^^") {
+                count++;
+            }
+        }
+    })
+    return count
+}
