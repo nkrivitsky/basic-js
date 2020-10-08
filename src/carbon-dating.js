aee5c0ccacaf1ebaad1df1cbef3d25c28dc2091a
Nikita krivitsky <nikdemoth1@gmail.com>
2020-10-08 13:58:20 +0300

    feat: implement simple-chain and carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+const PARAM = 0.693;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(value) {
+    if (value === undefined || value === null || typeof value !== typeof 'string' || value == "" || Number.isNaN(parseFloat(value)) ||
+        Number.parseFloat(value) <= 0) return false
+        /*throw new CustomError('Not implemented');
+        // remove line with error and write your code here*/
+    let result = Math.log10(MODERN_ACTIVITY / parseFloat(value)) / PARAM * HALF_LIFE_PERIOD
+
+    return result < 0 ? false : result
 };
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,56 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    checkInternal() {
+        if (this['array'] == undefined) {
+            this['array'] = []
+        }
+    },
+    getLength() {
+        this.checkInternal()
+
+        return this.array.length
+    },
+    addLink(value) {
+        this.checkInternal()
+        let str = ''
+        if (value === undefined) {
+            str = ''
+        } else if (value === null) {
+            str = 'null'
+        } else {
+            str = value.toString()
+        }
+        this.array.push('( ' + str + ' )')
+
+        return this
+    },
+    removeLink(position) {
+        this.checkInternal()
+
+        if (!Number.isInteger(position) || position - 1 < 0 || position > this.array.length) throw new Error('No such possition')
+
+        this.array.splice(position - 1, 1);
+
+        return this
+    },
+    reverseChain() {
+        this.checkInternal();
+
+        this.array = this.array.reverse();
+
+        return this;
+    },
+    finishChain() {
+        this.checkInternal()
+        if (this.array.length == 0) return ""
+
+        let str = this.array.join("~~")
+
+        this.array = []
+
+        return str
+    }
 };
 
 module.exports = chainMaker;
