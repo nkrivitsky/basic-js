2fba07e4d46d8702e62eea2deba98bb6f952fad3
Nikita krivitsky <nikdemoth1@gmail.com>
2020-10-12 11:52:38 +0300

    feat: repeater

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -7,8 +7,7 @@ const PARAM = 0.693;
 module.exports = function dateSample(value) {
     if (value === undefined || value === null || typeof value !== typeof 'string' || value == "" || Number.isNaN(parseFloat(value)) ||
         Number.parseFloat(value) <= 0) return false
-        /*throw new CustomError('Not implemented');
-        // remove line with error and write your code here*/
+
     let result = Math.log10(MODERN_ACTIVITY / parseFloat(value)) / PARAM * HALF_LIFE_PERIOD
 
     return result < 0 ? false : result
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,50 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    if (str === undefined) return "";
+
+    if (str === null) str = 'null'
+
+    if (options === undefined || options === null) {
+        options = new Object();
+    }
+
+    if (options["repeatTimes"] === undefined) {
+        options["repeatTimes"] = 1;
+    }
+
+    if (options["additionRepeatTimes"] === undefined) {
+        options["additionRepeatTimes"] = 1;
+    }
+
+    if (options["addition"] === undefined) {
+        options["addition"] = '';
+    }
+
+    if (options["addition"] === null) {
+        options["addition"] = 'null';
+    }
+
+    if (options["separator"] === undefined) {
+        options["separator"] = '+';
+    }
+
+    if (options["separator"] === null) {
+        options["separator"] = 'null';
+    }
+
+    if (options["additionSeparator"] === undefined) {
+        options["additionSeparator"] = '|';
+    }
+
+    if (options["additionSeparator"] === null) {
+        options["additionSeparator"] = 'null';
+    }
+
+    console.log(options);
+    let additionStr = ([].concat(...Array(Number.parseInt(options["additionRepeatTimes"])).fill(options["addition"].toString())).join(options["additionSeparator"].toString()));
+
+    let array = [].concat(...Array(Number.parseInt(options["repeatTimes"])).fill(str.toString() + additionStr));
+
+    return array.join(options["separator"].toString());
 };
-  
